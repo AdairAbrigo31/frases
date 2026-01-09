eed1ac3ae4c6fe8f7d88047baee7fc3da537c16d
adair.abrigo <adair.abrigo@tia.com.ec>
2026-01-08 23:34:37 -0500

    feat: endpoint health

--- frases/urls.py
+++ frases/urls.py
@@ -2,11 +2,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import WorkViewSet, QuoteViewSet
+from django.http import JsonResponse
 
 router = DefaultRouter()
 router.register('works', WorkViewSet, basename='work')
 router.register('quotes', QuoteViewSet, basename='quote')
 
+def health(request):
+    return JsonResponse({"status": "ok"})
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('health/', health)
 ]
