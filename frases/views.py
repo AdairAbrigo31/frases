3a2cefee8c0ffd0f7787bd2f795545bc1978dfa3
adair <adairaxe31@hotmail.com>
2026-01-10 00:19:14 -0500

    feat: route for create work totaly public

--- frases/views.py
+++ frases/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Work, Quote
 from .serializers import WorkSerializer, QuoteSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class WorkViewSet(viewsets.ModelViewSet):
     queryset = Work.objects.all()
     serializer_class = WorkSerializer
+    def get_permissions(self):
+        # POST y GET son públicos
+        if self.action in ['create', 'list', 'retrieve']:
+            return [AllowAny()]
+        # PUT, PATCH, DELETE requieren autenticación
+        return [IsAuthenticated()]
 
 class QuoteViewSet(viewsets.ModelViewSet):
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+    def get_permissions(self):
+        if self.action in ['create', 'list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
