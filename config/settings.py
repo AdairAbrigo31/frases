2fc2961b090f2182f992a0afd882223971dff572
adair <adairaxe31@hotmail.com>
2026-01-11 14:42:20 -0500

    feat: allow request to our frontend

--- config/settings.py
+++ config/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'frases'
+    'frases',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://dnig82chvwnmx.cloudfront.net",
+    "http://dnig82chvwnmx.cloudfront.net"
 ]
 
 ROOT_URLCONF = 'config.urls'
